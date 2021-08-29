7ea5e9f5927721eeff6bb906b5620b9865ab2e0f
will14lee <will14lee@gmail.com>
2021-08-29 15:42:01 -0400

    commit tree 4fdac6de8c2b433c84fe1ee4e81d87266b19c038

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,34 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-
+    self.end_with?("!")
   end
-
+  
   def count_sentences
-
+    count= 0
+    word_count= self.split(" ")
+    word_count.each do |n|
+      if n.question? || n.sentence? || n.exclamation?
+        count += 1
+      end
+    end
+    puts "There are #{count} sentences!"
+    count
   end
 end
+
+
+hello= String.new("This is a string! It has three sentences. Right?")
+hello.count_sentences
+# puts "Who are you?".split(" ")
+# binding.pry
+
